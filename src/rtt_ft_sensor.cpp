198800f272b38ae2f8e54a6d41aaa73a4442c809
Antoine Hoarau <hoarau.robotics@gmail.com>
2015-04-10 17:07:23 +0200

    Now socket connect is in Init()

--- src/rtt_ft_sensor.cpp
+++ src/rtt_ft_sensor.cpp
@@ -1,13 +1,12 @@
 #include "rtt_ati_sensor/rtt_ft_sensor.hpp"
 
 rtt_ati::FTSensor::FTSensor(std::string const& name) : TaskContext(name){
-  this->addProperty("ip",ip_).doc("(xxx.xxx.xxx.xxx) The IP address for the ATI NET F/T box (default : "+ati::default_ip+" )");
-  this->ports()->addPort("wrench",this->port_WrenchStamped);
-  port_WrenchStamped.createStream(rtt_roscomm::topic(this->getName() +"/data"));
-  this->addOperation("setBias",&rtt_ati::FTSensor::setBias,this,RTT::OwnThread);
-  this->addProperty( "calibration_index", calibration_index_).doc("(uint) The calibration index to use (default: current)");
-  calibration_index_ = ati::current_calibration;
-  ip_ = "";
+    this->addProperty("ip",ip_).doc("(xxx.xxx.xxx.xxx) The IP address for the ATI NET F/T box (default : "+ati::default_ip+" )");
+    this->ports()->addPort("wrench",this->port_WrenchStamped);
+    port_WrenchStamped.createStream(rtt_roscomm::topic(this->getName() +"/data"));
+    this->addOperation("setBias",&rtt_ati::FTSensor::setBias,this,RTT::OwnThread);
+    this->addProperty( "calibration_index", calibration_index_).doc("(uint) The calibration index to use (default: current)");
+    ft_sensor_ = boost::shared_ptr<ati::FTSensor>(new ati::FTSensor());
 }
 bool rtt_ati::FTSensor::setBias(std_srvs::Empty::Request& request, std_srvs::Empty::Response& response)
 {
@@ -16,37 +15,51 @@ bool rtt_ati::FTSensor::setBias(std_srvs::Empty::Request& request, std_srvs::Emp
 }
 
 bool rtt_ati::FTSensor::configureHook(){
-    bool all_configured = false;
+    calibration_index_ = ati::current_calibration;
+    ip_ = "";
+    bool configured = false;
+    
     RTT::log().setLogLevel(RTT::Logger::Warning);
     
+    //  Setting the ROS Service set_bias
     boost::shared_ptr<rtt_rosservice::ROSService> rosservice = this->getProvider<rtt_rosservice::ROSService>("rosservice");
-    rosservice->connect("setBias",this->getName()+"/set_bias","std_srvs/Empty");
     
+    if(rosservice)
+        rosservice->connect("setBias",this->getName()+"/set_bias","std_srvs/Empty");
+    else
+        RTT::log(RTT::Warning) << "ROSService not available" << RTT::endlog();
+    
+    //  Getting params from the parameter server,  if available
     boost::shared_ptr<rtt_rosparam::ROSParam> rosparam = this->getProvider<rtt_rosparam::ROSParam>("rosparam");
     
     if(!ip_.empty())
         RTT::log(RTT::Warning)<<"Orocos specified ip : " << ip_<<RTT::endlog();
     
     if(rosparam){
-        all_configured = rosparam->getComponentPrivate("ip");
-        if(!all_configured && ip_.empty())
+        configured = rosparam->getComponentPrivate("ip");
+        if(!configured && ip_.empty())
         {
             ip_ = ati::default_ip;
             RTT::log(RTT::Warning)<<"ROSParam available, but couldn't get ip parameter, using default ip : " << ati::default_ip<<RTT::endlog();
         }else{
-            RTT::log(RTT::Info)<<"ROSparam ip provided : " << ip_ <<" ,using this."<<RTT::endlog();
+            RTT::log(RTT::Info)<<"ROSparam ip provided,  using " << ip_ <<RTT::endlog();
+        }
+        configured = rosparam->getComponentPrivate("calibration_index");
+        if(!configured && calibration_index_ ==  ati::current_calibration)
+        {
+            RTT::log(RTT::Warning)<<"ROSParam available, but couldn't get calibration index parameter, using current calibration " <<RTT::endlog();
+        }else{
+            RTT::log(RTT::Info)<<"ROSparam calibration index provided,  using " << calibration_index_ <<RTT::endlog();
         }
     }else if(ip_.empty()){
         ip_ = ati::default_ip;
         RTT::log(RTT::Warning)<<"ROSParam unavailable and no IP specified, using default ip : " << ati::default_ip<<RTT::endlog();
     }
     
+    
     this->port_WrenchStamped.setDataSample(this->wrenchStamped);
-    
-    ft_sensor_ = boost::shared_ptr<ati::FTSensor>(new ati::FTSensor(ip_,calibration_index_));
-    
-    all_configured = ft_sensor_->init(ati::command_s::REALTIME);
-    return all_configured;
+    configured = ft_sensor_->init(ip_,calibration_index_,ati::command_s::REALTIME);
+    return configured;
 }
 
 void rtt_ati::FTSensor::updateHook(){
